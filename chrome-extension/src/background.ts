579ca007d8f3e9bea89bface6880a6f1435dec3d
Kaporos <theo@daron.be>
2022-07-07 22:48:29 +0200

    feat(extension): ✨ Redirecting user to created tab only when loading complete !

--- chrome-extension/src/background.ts
+++ chrome-extension/src/background.ts
@@ -4,9 +4,28 @@ chrome.runtime.onMessage.addListener(function(message: Message, sender, sendResp
     switch (message.type) {
         case MessageType.OpenTab:
             const data = message as OpenTabMessage
-            chrome.tabs.create({
-                url: data.url,
-                active: false
-            })
+            openTab(data.url)
     }
 });
+
+
+function openTab(url: string) {
+    chrome.tabs.create({
+        url,
+        active: false
+    }).then((tab) => {
+        const changeInterval = setInterval(async () => {
+            if (!tab.id) {
+                return
+            }
+            const updatedTab = await chrome.tabs.get(tab.id)
+            console.log(updatedTab)
+            if (updatedTab.status == "complete") {
+                chrome.tabs.highlight({
+                    tabs: [tab.index]
+                })
+                clearInterval(changeInterval)
+            }
+        }, 500)
+    })
+}
